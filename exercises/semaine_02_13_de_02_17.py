35b7f0a10a7adbdc74e636794e5d6448d3a766a4
nataliasviattseva <nataliasviattseva@gmail.com>
2023-02-14 12:18:01 +0100

    ajoutées les exercises de mardi 14 février

--- exercises/semaine_02_13_de_02_17.py
+++ exercises/semaine_02_13_de_02_17.py
@@ -262,3 +262,58 @@ if __name__ == '__main__':
     print("BLABLA")
     # cb2 + cb2  # TypeError: unsupported operand type(s) for +=: 'float' and 'Compte'
     print(f"Solde : {cb2.solde:.2f}")
+
+
+from tkinter import *
+
+
+class EnterMessage(Frame):
+    """Classe EnterMessage  (Frame de saisie du message)
+    Cette classe dérive de la classe Tkinter.Frame"""
+
+    def __init__(self, master=None):
+        """Initialisation : création d'un widget Frame"""
+        Frame.__init__(self, master, bg='navy')
+        self.pack(padx=10, pady=10)
+        self.CreateWidgets()
+
+    def CreateWidgets(self):
+        """Création des widgets Entry et Button dans le widget Frame"""
+        self.NouveauMessage = StringVar()
+        Entry(master=self, textvariable=self.NouveauMessage).pack(side=LEFT, padx=10, pady=10)
+        Button(master=self, text="Nouveau", fg="navy", command=self.nouveau).pack(padx=10, pady=10)
+
+    def nouveau(self):
+        """Création d'une instance de la classe NewPostIt"""
+        if self.NouveauMessage.get() != "":
+            NewPostIt(master=self.master, message=self.NouveauMessage.get())
+            self.NouveauMessage.set("")
+
+
+class NewPostIt(Frame):
+    """Classe post-it (Frame Post-It)
+    Cette classe dérive de la classe Tkinter.Frame"""
+
+    def __init__(self, master=None, message=None):
+        """Initialisation : création d'un widget Frame"""
+        Frame.__init__(self, master, bg='grey')
+        self.pack(side=LEFT, padx=10, pady=10)
+        self.CreateWidgets(message)
+
+    def CreateWidgets(self, message):
+        """Création des widgets Label et Button dans le widget Frame"""
+        Label(master=self, text=message, fg="maroon", bg='white').pack(padx=10, pady=10)
+        Button(master=self, text="Effacer", fg="navy", command=self.destroy).pack(padx=10, pady=10)
+
+
+if __name__ == '__main__':
+    # création de la fenêtre principale
+    mafenetre = Tk()
+    mafenetre.title("Post-it")
+
+    mafenetre['bg'] = "blue"
+
+    # Création d'une instance de la classe EnterMessage
+    EnterMessage(master=mafenetre)
+
+    mafenetre.mainloop()
